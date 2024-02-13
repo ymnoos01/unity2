7563730683d3b05ae538b559bbf2e7710615ae29
BLOODWOLFGR <shopdanger02@gmail.com>
2024-02-13 20:28:27 +0200

    flashlight on off

---
+++ project2unity/Assets/scripts/FlashlightController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    public Light flashlight; // Reference to the flashlight light source
+    private bool isFlashlightOn = false;
+
+    void Start()
+    {
+        flashlight.enabled = false; // Ensure flashlight is initially off
+    }
+
+    void Update()
+    {
+        // Check for input to toggle flashlight
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleFlashlight();
+        }
+    }
+
+    void ToggleFlashlight()
+    {
+        isFlashlightOn = !isFlashlightOn; // Toggle flashlight state
+
+        // Enable/disable flashlight accordingly
+        flashlight.enabled = isFlashlightOn;
+
+        // You can add additional effects or behavior here, like sound effects or animations
+    }
+}
