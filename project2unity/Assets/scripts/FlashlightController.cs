0587f988cdfd680f054fae88533615d212dab445
ymnoos01 <134231282+ymnoos01@users.noreply.github.com>
2024-03-20 19:12:35 +0200

    spotlight part 1

--- project2unity/Assets/scripts/FlashlightController.cs
+++ project2unity/Assets/scripts/FlashlightController.cs
@@ -1,33 +1,3 @@
-// using UnityEngine;
-
-// public class FlashlightController : MonoBehaviour
-// {
-//     public Light flashlight; // Reference to the flashlight light source
-//     private bool isFlashlightOn = false;
-
-//     void Start()
-//     {
-//         flashlight.enabled = false; // Ensure flashlight is initially off
-//     }
-
-//     void Update()
-//     {
-//         // Check for input to toggle flashlight
-//         if (Input.GetKeyDown(KeyCode.E))
-//         {
-//             ToggleFlashlight();
-//         }
-//     }
-
-//     void ToggleFlashlight()
-//     {
-//         isFlashlightOn = !isFlashlightOn; // Toggle flashlight state
-
-//         // Enable/disable flashlight accordingly
-//         flashlight.enabled = isFlashlightOn;
-//     }
-// }
-
 using UnityEngine;
 
 public class FlashlightController : MonoBehaviour
@@ -57,10 +27,94 @@ public class FlashlightController : MonoBehaviour
         flashlight.enabled = isFlashlightOn;
     }
 
-    public void DeactivateSphere()
+    public void DeactivateSphere(GameObject sphere)
     {
         // Deactivate the sphere
-        gameObject.SetActive(false);
-        Debug.Log("Sphere is being deactivated."); // Debug log
+        Destroy(sphere);
+        Debug.Log("Sphere is being deactivated."); // Debug log  
     }
 }
+
+// using UnityEngine;
+
+// public class FlashlightController : MonoBehaviour
+// {
+//     public Light flashlight; // Reference to the flashlight light source
+//     private bool isFlashlightOn = false;
+
+//     void Start()
+//     {
+//         flashlight.enabled = false; // Ensure flashlight is initially off
+//     }
+
+//     void Update()
+//     {
+//         // Check for input to toggle flashlight
+//         if (Input.GetKeyDown(KeyCode.E))
+//         {
+//             ToggleFlashlight();
+//         }
+//     }
+
+//     void ToggleFlashlight()
+//     {
+//         isFlashlightOn = !isFlashlightOn; // Toggle flashlight state
+
+//         // Enable/disable flashlight accordingly
+//         flashlight.enabled = isFlashlightOn;
+//     }
+
+//     public bool IsFlashlightOn()
+//     {
+//         return isFlashlightOn;
+//     }
+
+//     public void DeactivateSphere(GameObject sphere)
+//     {
+//         // Deactivate the sphere
+//         Destroy(sphere);
+//         Debug.Log("Sphere is being deactivated."); // Debug log  
+//     }
+// }
+
+// using UnityEngine;
+
+// public class FlashlightController : MonoBehaviour
+// {
+//     public Light flashlight; // Reference to the flashlight light source
+//     private bool isFlashlightOn = false;
+
+//     void Start()
+//     {
+//         flashlight.enabled = false; // Ensure flashlight is initially off
+//     }
+
+//     void Update()
+//     {
+//         // Check for input to toggle flashlight
+//         if (Input.GetKeyDown(KeyCode.E))
+//         {
+//             ToggleFlashlight();
+//         }
+//     }
+
+//     void ToggleFlashlight()
+//     {
+//         isFlashlightOn = !isFlashlightOn; // Toggle flashlight state
+
+//         // Enable/disable flashlight accordingly
+//         flashlight.enabled = isFlashlightOn;
+//     }
+
+//     public bool IsFlashlightOn()
+//     {
+//         return isFlashlightOn;
+//     }
+
+//     public void DeactivateSphere(GameObject sphere)
+//     {
+//         // Deactivate the sphere
+//         Destroy(sphere);
+//     }
+// }
+
--- project2unity/Assets/scripts/SphereDisappearance.cs
+++ project2unity/Assets/scripts/SphereDisappearance.cs
@@ -1,79 +1,3 @@
-// using UnityEngine;
-
-// public class SphereDisappearance : MonoBehaviour
-// {
-//     public GameObject sphere; // Reference to the sphere game object
-//     public Light flashlight; // Reference to the flashlight light source
-
-//     void Update()
-//     {
-//         if (IsSpotlightTouchingSphere())
-//         {
-//             sphere.SetActive(false); // Deactivate the sphere
-//         }
-//     }
-
-//     bool IsSpotlightTouchingSphere()
-//     {
-//         RaycastHit hit;
-//         Vector3 spotlightDirection = flashlight.transform.forward;
-
-//         if (Physics.Raycast(flashlight.transform.position, spotlightDirection, out hit))
-//         {
-//             if (hit.collider.gameObject == sphere)
-//             {
-//                 Debug.Log("Spotlight is touching the sphere!"); // Debug log
-//                 return true; // The spotlight is touching the sphere
-//             }
-//         }
-
-//         return false; // The spotlight is not touching the sphere
-//     }
-// }
-
-
-
-
-
-//22222222222222222222222222222222
-
-
-
-
-// using UnityEngine;
-
-// public class SphereDisappearance : MonoBehaviour
-// {
-//     private bool isSpotlightTouchingSphere = false;
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
-//         {
-//             isSpotlightTouchingSphere = true;
-//             Debug.Log("Spotlight has entered the sphere collider."); // Debug log
-//         }
-//     }
-
-//     private void OnTriggerExit(Collider other)
-//     {
-//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
-//         {
-//             isSpotlightTouchingSphere = false;
-//             Debug.Log("Spotlight has exited the sphere collider."); // Debug log
-//         }
-//     }
-
-//     void Update()
-//     {
-//         if (isSpotlightTouchingSphere)
-//         {
-//             gameObject.SetActive(false); // Deactivate the sphere
-//             Debug.Log("Sphere is being deactivated."); // Debug log
-//         }
-//     }
-// }
-
 using UnityEngine;
 
 public class SphereDisappearance : MonoBehaviour
@@ -91,10 +15,28 @@ public class SphereDisappearance : MonoBehaviour
         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
         {
             isSpotlightTouchingSphere = true;
-            flashlightController.DeactivateSphere(); // Call the method to deactivate the sphere from FlashlightController
+            flashlightController.DeactivateSphere(other.gameObject); // Call the method to deactivate the sphere from FlashlightController
             Debug.Log("Spotlight has entered the sphere collider."); // Debug log
         }
     }
+
+    // private void OnTriggerEnter(Collider other)
+    //  {
+    //      if (other.gameObject.CompareTag("Sphere")) 
+    //      {
+    //          isSpotlightTouchingSphere = true;
+
+    //          if (flashlightController.isFlashlightOn())
+    //          {
+    //              flashlightController.DeactivateSphere(other.gameObject); 
+    //              Debug.Log("Spotlight has entered the sphere collider. Sphere deactivated."); 
+    //          }
+    //          else
+    //          {
+    //              Debug.Log("Spotlight has entered the sphere collider, but the flashlight is off. Sphere not deactivated."); 
+    //          }
+    //      }
+    //  }
 
     private void OnTriggerExit(Collider other)
     {
@@ -105,3 +47,115 @@ public class SphereDisappearance : MonoBehaviour
         }
     }
 }
+
+//222222222
+
+// using UnityEngine;
+
+// public class SphereDisappearance : MonoBehaviour
+// {
+//     private FlashlightController flashlightController; // Reference to the FlashlightController script
+//     private bool isSpotlightTouchingSphere = false;
+
+//     private void Start()
+//     {
+//         flashlightController = FindObjectOfType<FlashlightController>(); // Find the FlashlightController in the scene
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
+//         {
+//             if (flashlightController != null && flashlightController.enabled) // Check if the flashlight is enabled
+//             {
+//                 isSpotlightTouchingSphere = true;
+//                 flashlightController.DeactivateSphere(other.gameObject); // Call the method to deactivate the sphere from FlashlightController
+//                 Debug.Log("Spotlight has entered the sphere collider."); // Debug log
+//             }
+//         }
+//     }
+
+//     private void OnTriggerExit(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
+//         {
+//             isSpotlightTouchingSphere = false;
+//             Debug.Log("Spotlight has exited the sphere collider."); // Debug log
+//         }
+//     }
+// }
+
+
+//333333333333333333333
+
+
+
+// using UnityEngine;
+
+// public class SphereDisappearance : MonoBehaviour
+// {
+//     private FlashlightController flashlightController; // Reference to the FlashlightController script
+//     private bool isSpotlightTouchingSphere = false;
+
+//     private void Start()
+//     {
+//         flashlightController = FindObjectOfType<FlashlightController>(); // Find the FlashlightController in the scene
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
+//         {
+//             if (flashlightController != null && flashlightController.IsFlashlightOn()) // Check if the flashlight is on
+//             {
+//                 isSpotlightTouchingSphere = true;
+//                 flashlightController.DeactivateSphere(other.gameObject); // Call the method to deactivate the sphere from FlashlightController
+//                 Debug.Log("Spotlight has entered the sphere collider."); // Debug log
+//             }
+//         }
+//     }
+
+//     private void OnTriggerExit(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
+//         {
+//             isSpotlightTouchingSphere = false;
+//             Debug.Log("Spotlight has exited the sphere collider."); // Debug log
+//         }
+//     }
+// }
+
+// using UnityEngine;
+
+// public class SphereDisappearance : MonoBehaviour
+// {
+//     private FlashlightController flashlightController; // Reference to the FlashlightController script
+//     private bool isSpotlightTouchingSphere = false;
+
+//     private void Start()
+//     {
+//         flashlightController = FindObjectOfType<FlashlightController>(); // Find the FlashlightController in the scene
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
+//         {
+//             if (flashlightController != null && flashlightController.IsFlashlightOn()) // Check if the flashlight is on
+//             {
+//                 isSpotlightTouchingSphere = true;
+//                 flashlightController.DeactivateSphere(other.gameObject); // Call the method to deactivate the sphere from FlashlightController
+//                 Debug.Log("Sphere is being deactivated."); // Debug log
+//             }
+//         }
+//     }
+
+//     private void OnTriggerExit(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Sphere")) // Assuming the sphere has the tag "Sphere"
+//         {
+//             isSpotlightTouchingSphere = false;
+//         }
+//     }
+// }
+
